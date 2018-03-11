bb7b06e2e7e8cd30cbaa18f95d4a62a7ac535378
kenny <knnyngo@gmail.com>
2018-03-10 17:46:35 -0800

    editing port

--- workouts.js
+++ workouts.js
@@ -6,7 +6,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', process.env.PORT || 3003);
+app.set('port', process.env.PORT || 3306);
 
 app.use('/', express.static(__dirname));
 
